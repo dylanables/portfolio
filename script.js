06c303e8856785f4b2b6b6f2f12e335673da1e20
Dylan Ables <dylanables@Dylans-MacBook-Pro.local>
2023-11-20 22:26:54 -0800

    Changed some graphics

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let mountain6 = document.getElementById('mountain6');
 let cloudsLeft = document.getElementById('clouds-left');
 let cloudsRight = document.getElementById('clouds-right');
 
+const [red, green, blue] = [19, 28, 52]
+const bg = document.querySelector('.parallax')
+
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
 
@@ -10,4 +13,11 @@ window.addEventListener('scroll', () => {
     mountain6.style.top = value * 2.5 + 'px';
     cloudsLeft.style.left = value * -1.5 + 'px';
     cloudsRight.style.left = value * 1.5 + 'px';
+
+    const y = 1 + (window.scrollY || window.pageYOffset) / 100
+    const [r, g, b] = [red*y, green*y, blue*y].map(Math.round)
+    bg.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+
+    var $nav = $(".navbar");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 })
