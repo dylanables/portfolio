339665caec0e91e7ce1b0e2fff946a3a801b8c56
Dylan Ables <dylanables@Dylans-MacBook-Pro.local>
2023-11-21 13:39:44 -0800

    Added responsiveness to navbar and parallax section

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let text = document.getElementById('text');
-let mountain6 = document.getElementById('mountain6');
+let moon = document.getElementById('moon');
 let cloudsLeft = document.getElementById('clouds-left');
 let cloudsRight = document.getElementById('clouds-right');
 let nav = document.getElementById('nav');
+let parallax = document.getElementById('parallax');
 let about = document.getElementById('about');
 
 const [red, green, blue] = [19, 28, 52]
@@ -11,8 +12,11 @@ const bg = document.querySelector('.parallax')
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
 
-    text.style.marginTop = value * 1.5 + 'px';
-    mountain6.style.top = value * 2.5 + 'px';
+    console.log("parallax height", parallax.offsetHeight);
+    console.log("value Y", value);
+
+    text.style.marginTop = Math.min(value * 1.5, about.offsetTop) + 'px';
+    moon.style.top = Math.min(value * 2.5, about.offsetTop) + 'px';
     cloudsLeft.style.left = value * -1.5 + 'px';
     cloudsRight.style.left = value * 1.5 + 'px';
 
